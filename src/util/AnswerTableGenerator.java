3074d1828eb9455f91ea9a9bb5d069b64449fe9a
Barath R <barathshun@gmail.com>
2014-03-23 03:36:49 +0530

    Edited TableGenerator 1

--- src/util/AnswerTableGenerator.java
+++ src/util/AnswerTableGenerator.java
@@ -10,46 +10,46 @@ public class AnswerTableGenerator {
 		String html = "";
 		
 			html += "<tr>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
+			html += "<td class=\"1\" id=\""+questionindex+"\">";
 			html += "";
 			html += "</td>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
-			html += "";
-			html += "</td>";
-			html += "</tr>";
-			
-			html += "<tr>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
-			html += "";
-			html += "</td>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
+			html += "<td class=\"2\" id=\""+questionindex+"\">";
 			html += "";
 			html += "</td>";
 			html += "</tr>";
 			
 			html += "<tr>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
+			html += "<td class=\"3\" id=\""+questionindex+"\">";
 			html += "";
 			html += "</td>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
-			html += "";
-			html += "</td>";
-			html += "</tr>";
-			
-			html += "<tr>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
-			html += "";
-			html += "</td>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
+			html += "<td class=\"4\" id=\""+questionindex+"\">";
 			html += "";
 			html += "</td>";
 			html += "</tr>";
 			
 			html += "<tr>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
+			html += "<td class=\"5\" id=\""+questionindex+"\">";
 			html += "";
 			html += "</td>";
-			html += "<td class=\"\" id=\""+questionindex+"\">";
+			html += "<td class=\"6\" id=\""+questionindex+"\">";
+			html += "";
+			html += "</td>";
+			html += "</tr>";
+			
+			html += "<tr>";
+			html += "<td class=\"7\" id=\""+questionindex+"\">";
+			html += "";
+			html += "</td>";
+			html += "<td class=\"8\" id=\""+questionindex+"\">";
+			html += "";
+			html += "</td>";
+			html += "</tr>";
+			
+			html += "<tr>";
+			html += "<td class=\"9\" id=\""+questionindex+"\">";
+			html += "";
+			html += "</td>";
+			html += "<td class=\"10\" id=\""+questionindex+"\">";
 			html += "";
 			html += "</td>";
 			html += "</tr>";
